38083af7a62e893c6e18ac7f29fa73b67cffe85e
ramthorve <ramthorve12@gmail.com>
2022-09-21 11:19:42 +0530

    getter setter for studentdetails added

---
+++ Quiz_Project/src/com/quizproject/grouph/StudentDetails.java
@@ -0,0 +1,23 @@
+package com.quizproject.grouph;
+
+
+public class StudentDetails {
+	private String studentRollNumber;
+	private String studentName;
+	
+//  Here we are providing public getter and setter method for private variable
+	
+	public String getRollNumber() {
+		return studentRollNumber;
+	}
+	public void setRollNumber(String studentRollNumber) {
+		this.studentRollNumber = studentRollNumber;
+	}
+	public String getName() {
+		return studentName;
+	}
+	public void setName(String studentName) {
+		this.studentName = studentName;
+	}	
+}
+
