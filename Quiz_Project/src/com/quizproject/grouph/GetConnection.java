e317f4f08ca1ff87483fd14365d56953dd79d96f
teju <tejaswinirathod24@gmail.com>
2022-09-21 16:51:19 +0530

    getConnection Class Modified

--- Quiz_Project/src/com/quizproject/grouph/GetConnection.java
+++ Quiz_Project/src/com/quizproject/grouph/GetConnection.java
@@ -4,21 +4,6 @@ import java.sql.*;
 
 public class GetConnection {
 
-<<<<<<< HEAD
-	Connection con=null;
-	//creating connection
-	public Connection getConnectionDetails() throws SQLException {
-		System.out.println("Connecting....");
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://localhost:3308/example", "root", "Sagar@142143");
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		return con;
-	}
-}
-=======
   Connection con=null;
   //creating connection
   public Connection getConnectionDetails() throws SQLException {
@@ -34,4 +19,4 @@ public class GetConnection {
 }
 
 
->>>>>>> FB-Tejaswini
+
