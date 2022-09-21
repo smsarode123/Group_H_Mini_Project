dfbe82b0970c9539561b61ab2db6c259c23f8c4a
Sagar Sarode <sagarsarode1997@gmail.com>
2022-09-21 11:40:18 +0530

    In GetConnection class data base connection created

--- Quiz_Project/src/com/quizproject/grouph/GetConnection.java
+++ Quiz_Project/src/com/quizproject/grouph/GetConnection.java
@@ -2,4 +2,16 @@ package com.quizproject.grouph;
 
 public class GetConnection {
 
+	Connection con=null;
+	//creating connection
+	public Connection getConnectionDetails() throws SQLException {
+		System.out.println("Connecting....");
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3308/example", "root", "Sagar@142143");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
 }
